5e16ae0572228e364069e4dd9992670adf23509f
3LOK <yoava@wix.com>
2015-07-26 17:26:45 +0300

    Support with changing api URL

--- index.js
+++ index.js
@@ -2,29 +2,36 @@ var Q = require("q");
 
 var openrest = require("./lib/openrest4js-client-1.1.1.js");
 
-var _client = new openrest.Client({apiUrl:"https://spice-prod.appspot.com/v1.1"});
+var apiUrl = null;
 
-exports.request = function(params) {
-    var request = params.request;
-    var callback = params.callback;
-    var client = params.client || _client;
+module.exports = {
+    request:function(params) {
+        var _client = new openrest.Client({apiUrl:(apiUrl || "https://spice-prod.appspot.com/v1.1")});
 
-    var defer = Q.defer();
+        var request = params.request;
+        var callback = params.callback;
+        var client = params.client || _client;
 
-    client.request({
-        request:request,
-        callback:function(e) {
-            if (e.error) {
-                defer.reject(e);
-            } else {
-                defer.resolve(e);
+        var defer = Q.defer();
+
+        client.request({
+            request:request,
+            callback:function(e) {
+                if (e.error) {
+                    defer.reject(e);
+                } else {
+                    defer.resolve(e);
+                }
+
+                if (callback) {
+                    callback(e);
+                }
             }
+        });
 
-            if (callback) {
-                callback(e);
-            }
-        }
-    });
-
-    return defer.promise;
+        return defer.promise;
+    },
+    setApiUrl:function(val) {
+        apiUrl = val;
+    }
 }
